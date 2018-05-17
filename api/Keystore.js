a8da20f727f5d0533e202211d5e91fbedbd38dea
coloro <colorolabs@gmail.com>
2018-05-17 13:22:45 -0400

    keystore

--- api/Keystore.js
+++ api/Keystore.js
@@ -1,10 +1,26 @@
-var get_keystore = function(req, res) {
-  console.log('get keystore')
-  success(res, 'keystore')
+var temp = []
+
+function get_keystore(req, res) {
+  console.log('get keystore', req.params)
+  let rut = req.params
+  let password = req.password
+
+  let ks = temp.find(ks => ks.rut === rut)
+  if (!ks || ks.password !== password) return fail(res, 'Parametros incorrectos')
+
+  success(res, ks)
 }
 
-var create_keystore = function(req, res) {
-  console.log('create keystore')
+function create_keystore(req, res) {
+  console.log('create keystore', req.params)
+  var rut = req.params.rut
+  var password = req.params.password
+  var addresses = req.params.addresses
+  var keystore = req.params.keystore
+
+  temp.push({
+    rut, password, addresses, keystore
+  })
   success(res, 'created')
 }
 
@@ -14,6 +30,22 @@ function success(res, data) {
     data: data
   }
   res.status(200).json(response)
+}
+
+function fail(res, msg) {
+  var response = {
+    status: 'fail',
+    data: msg
+  }
+  res.status(400).json(response)
+}
+
+function error(res, msg) {
+  var response = {
+    status: 'error',
+    data: msg
+  }
+  res.status(500).json(response)
 }
 
 module.exports = {
--- app.js
+++ app.js
@@ -7,4 +7,5 @@ app.get('/', (req, res) => res.send('keyserver'))
 app.post('/keystore/create', keystore.create_keystore)
 app.post('/keystore/get', keystore.get_keystore)
 
-app.listen(3001, () => console.log('Keyserver listening on port 3001!'))
+var port = process.env.PORT || 4000
+app.listen(port, () => console.log('Keyserver listening on port ' + port))
